1991a47909360da66259af34fb23520783b6b055
Anna Atkinson <atkinsonsoftware@gmail.com>
2022-02-20 16:23:58 -0800

    Add method to Parse and return valid JSON.

--- data_parse.py
+++ data_parse.py
@@ -1,4 +1,18 @@
 import argparse
+import json
+import jsonschema
+
+def ParseJSON(jsonstr, schemastr):
+    try:
+        to_validate = json.loads(jsonstr)
+        to_validate_against = json.loads(schemastr)
+        jsonschema.validate(to_validate, to_validate_against)
+    except Exception as e:
+        print("Exception encountered: %s" % (e, ))
+        print("Invalid JSON data")
+        return None
+
+    return to_validate
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process file data")
@@ -8,5 +22,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     json_file = args.JSON_data
     json_schema = args.JSON_schema
+
+    valid_JSON_obj = ParseJSON(json_file.read(), json_schema.read())
+    if (valid_JSON_obj):
+        print(valid_JSON_obj)
+
     json_file.close()
     json_schema.close()
